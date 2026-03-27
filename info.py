73b4f2dd46c2b57fdc2d6a98ee0411e4395acc68
bro096316-web <bro096316@gmail.com>
2026-03-27 09:45:17 +0530

    Change LOG_API_CHANNEL default value to empty string

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ CHANNELS = [
 AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1002520793005"))
 AUTH_REQ_CHANNEL = int(environ.get("AUTH_REQ_CHANNEL", "-1002520793005"))
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002552925944"))
-LOG_API_CHANNEL = int(environ.get("LOG_API_CHANNEL", "https://t.me/+pVngOMgFHIZhNTE1"))
+LOG_API_CHANNEL = int(environ.get("LOG_API_CHANNEL", ""))
 LOG_VR_CHANNEL = int(environ.get("LOG_VR_CHANNEL", ""))
 
 # MongoDB
