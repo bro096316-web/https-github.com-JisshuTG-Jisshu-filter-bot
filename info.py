45dfd171ac66c17eea5270f7f2a333bf1f2e72f0
bro096316-web <bro096316@gmail.com>
2026-03-27 15:17:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,8 +41,7 @@ CHANNELS = [
 AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1002520793005"))
 AUTH_REQ_CHANNEL = int(environ.get("AUTH_REQ_CHANNEL", "-1002520793005"))
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002552925944"))
-LOG_API_CHANNEL = int(environ.get("LOG_API_CHANNEL") or 0)
-LOG_VR_CHANNEL = int(environ.get("LOG_VR_CHANNEL", "") or 0)
+
 
 # MongoDB
 DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://ABHI:ABHI@cluster0.h1ewpln.mongodb.net/?appName=Cluster0")
